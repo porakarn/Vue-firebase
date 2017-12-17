8902b3dcaf33f9f2fc86b49f4b11d73e98106afa
porakarn <porakarnsoon@gmail.com>
2017-12-18 00:38:12 +0700

    Start

---
+++ src/components/Home.vue
@@ -0,0 +1,69 @@
+<template>
+<div>
+      <section>
+          <v-parallax src="https://www.jobtutorhome.com/static/images/banner-01.jpg" height="400">
+            <v-layout
+              column
+              align-center
+              justify-center
+              class="white--text"
+            >
+            
+              <h1 class="white--text mb-2 display-1 text-xs-center">Parallax Template</h1>
+              <div class="subheading mb-3 text-xs-center">Powered by Vuetify</div>
+              <v-btn
+                class="blue lighten-2 mt-5"
+                dark
+                large
+                href="/pre-made-themes"
+              >
+                Get Started
+              </v-btn>
+            </v-layout>
+          </v-parallax>
+        </section>
+     
+
+  <v-container>
+    <v-layout row wrap>
+        <v-flex xs12 sm6 class="text-xs-center text-sm-right">
+        <v-btn large router to="/register" class="info">สมัครเรียน</v-btn>
+      </v-flex>
+        <v-flex xs12 sm6 class="text-xs-center text-sm-left">
+        <v-btn large router to="/board" class="info">ดูงาน</v-btn>
+      </v-flex>
+     
+    </v-layout>
+    
+  </v-container>
+</div>
+</template>
+
+<script>
+  export default {
+    computed: {
+      meetups () {
+        return this.$store.getters.featuredMeetups
+      },
+      loading () {
+        return this.$store.getters.loading
+      }
+    },
+    methods: {
+      onLoadMeetup (id) {
+        this.$router.push('/meetups/' + id)
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .title {
+    position: absolute;
+    bottom: 50px;
+    background-color: rgba(0,0,0,0.5);
+    color: white;
+    font-size: 2em;
+    padding: 20px;
+  }
+</style>
---
+++ src/components/Student/Register.vue
@@ -0,0 +1,218 @@
+<template>
+  <v-container>
+    <v-layout row>
+      <v-flex xs12 sm6 offset-sm3>
+        <h5>สมัครเรียน</h5>
+      </v-flex>
+    </v-layout>
+    <v-layout row>
+      <v-flex xs12>
+      <form @submit.prevent="onCreateMeetup">
+          <v-layout row>
+            <v-flex xs12 sm6 offset-sm3>
+              <v-text-field
+                name="name"
+                label="ชื่อ-นามสกุล นักเรียน"
+                id="name"
+                v-model="Course.name"
+                required></v-text-field>
+            </v-flex>
+          </v-layout>
+          <v-layout row>
+            <v-flex xs12 sm6 offset-sm3>
+              <v-text-field
+                name="phone"
+                label="เบอร์ติดต่อ"
+                id="phone"
+                v-model="Course.phone"
+                required></v-text-field>
+            </v-flex>
+          </v-layout>
+           <v-layout row>
+            <v-flex xs12 sm6 offset-sm3>
+              <v-text-field
+                name="line"
+                label="Line Id"
+                id="line"
+                v-model="Course.line"
+                required></v-text-field>
+            </v-flex>
+          </v-layout>
+           <v-layout row>
+            <v-flex xs12 sm6 offset-sm3>
+              <v-text-field
+                name="school"
+                label="สถานการศึกษา"
+                id="school"
+                v-model="Course.school"
+                required></v-text-field>
+            </v-flex>
+          </v-layout>
+            <v-layout row>
+            <v-flex xs6 sm4 offset-sm3>
+              <v-text-field
+                name="grade"
+                label="ระดับชั้น"
+                id="grade"
+                v-model="Course.grade"
+                required></v-text-field>
+            </v-flex>
+          </v-layout>
+          <br>
+    <v-layout row>
+      <v-flex xs12 sm6 offset-sm3>
+           <h5>Step 2 หลักสูตรที่ต้องการเรียน</h5>
+      </v-flex>
+    </v-layout>
+
+ <v-layout row>
+            <v-flex xs12 sm6 offset-sm3>
+              <v-text-field
+                name="subject"
+                label="วิชาที่เรียน"
+                id="subject"
+                v-model="Course.subject"
+                required></v-text-field>
+            </v-flex>
+          </v-layout>
+
+ <v-layout row>
+            <v-flex xs6 sm2 offset-sm3>
+              <v-text-field
+                name="student"
+                label="นักเรียน อยุ่ชั้นไหน"
+                id="student"
+                v-model="Course.student"
+                required></v-text-field>
+            </v-flex>
+          </v-layout>
+
+ <v-layout row>
+            <v-flex xs12 sm6 offset-sm3>
+              <v-text-field
+                name="tutor"
+                label="รายละเอียด ติวเตอร์"
+                id="tutor"
+                v-model="Course.tutor"
+                required></v-text-field>
+            </v-flex>
+          </v-layout>
+
+           <v-layout row>
+            <v-flex xs12 sm6 offset-sm3>
+              <v-text-field
+                name="location"
+                label="สถานที่เรียน"
+                id="location"
+                v-model="Course.location"
+                required></v-text-field>
+            </v-flex>
+          </v-layout>
+
+   <v-layout row>
+            <v-flex xs12 sm6 offset-sm3>
+              <v-text-field
+                name="date"
+                label="รายละเอียดวันที่เรียน เวลา"
+                id="date"
+                v-model="Course.date"
+                required></v-text-field>
+            </v-flex>
+          </v-layout>
+
+        
+          <br><br>
+    
+     
+           
+        
+          <v-layout row>
+            <v-flex xs12 sm6 offset-sm3>
+              <v-btn
+                class="primary"
+             
+                type="submit" >เรียบร้อย </v-btn>
+            </v-flex>
+          </v-layout>
+        </form>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+import axios from 'axios';
+      export default {
+    data () {
+      return {
+           e1: 0,
+          Course: { 
+        name: '',
+        phone: '',
+        line: '',
+        school: '',
+        grade:'',     
+        subject:'',
+        student:'',
+        tutor:'',
+        location:'',
+        date:'',
+        id:''
+              }
+        
+    
+      }
+    },
+    computed: {
+      formIsValid () {
+        return this.Course.name !== '' &&
+          this.Course.phone !== '' &&
+          this.Course.line !== '' &&
+          this.Course.school !== '' &&
+          this.Course.grade !== '' &&
+          this.Course.subject !== '' &&
+          this.Course.student !== '' &&
+          this.Course.tutor !== '' &&
+          this.Course.location !== '' &&
+          this.Course.date !== ''       
+      }},
+    methods: {
+     
+    
+ onCreateMeetup () {
+     
+     if (!this.formIsValid) {
+          return
+        }
+
+ let meetupData = {
+        
+          name: this.Course.name,
+          phone: this.Course.phone,
+          line: this.Course.line,
+          school: this.Course.school,
+          grade: this.Course.grade,
+          subject: this.Course.subject,
+          student: this.Course.student,
+          tutor: this.Course.tutor,
+          location: this.Course.location,
+          date: this.Course.date,
+          id: 'A' + Math.floor(Math.random()*10000)
+      }
+      console.log(meetupData);
+
+
+       axios.post('https://tutor-8e729.firebaseio.com/student.json', meetupData)
+          .then(res => console.log(res))
+          .catch(error => console.log(error))
+    }
+
+
+
+   
+      }
+      
+      
+    }
+  
+</script>
